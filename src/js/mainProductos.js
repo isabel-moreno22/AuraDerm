d8ef349a00cded42a7647db86c78887d0e6b6d78
andresmunra <andresmunra83@gmail.com>
2024-05-30 16:45:21 -0500

    tarea 7

---
+++ src/js/mainProductos.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/AuraDerm/productos.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const productContainer = document.getElementById("productContainer");
+      data.forEach((product) => {
+        addProduct(
+          productContainer,
+          product.id,
+          product.name,
+          product.img,
+          product.description,
+          product.price
+        );
+      });
+    })
+    .catch((error) => console.error("Error al cargar el JSON:", error));
+});
+
+function addProduct(container, id, name, imgSrc, description, price) {
+  const productDiv = document.createElement("div");
+  productDiv.classList.add("product");
+
+  const productImageContainer = document.createElement("div");
+  productImageContainer.classList.add("product-image");
+
+  const productImage = document.createElement("img");
+  productImage.src = imgSrc;
+  productImage.alt = name;
+
+  const productDetails = document.createElement("div");
+  productDetails.classList.add("product-details");
+
+  const productName = document.createElement("h2");
+  productName.textContent = name;
+
+  const productDescription = document.createElement("p");
+  productDescription.textContent = description;
+
+  const productPrice = document.createElement("p");
+  productPrice.textContent = `Precio: $${price}`;
+
+  const addToCartButton = document.createElement("button");
+  addToCartButton.textContent = "Agregar al carrito";
+
+  productImageContainer.appendChild(productImage);
+  productDetails.appendChild(productName);
+  productDetails.appendChild(productDescription);
+  productDetails.appendChild(productPrice);
+  productDetails.appendChild(addToCartButton);
+
+  productDiv.appendChild(productImageContainer);
+  productDiv.appendChild(productDetails);
+
+  container.appendChild(productDiv);
+}
